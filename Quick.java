cce9e779130511c03c87b94994205401d4cb85e2
bertw2002 <awan00@stuy.edu>
2019-03-11 18:05:36 -0400

    partition works now

--- Quick.java
+++ Quick.java
@@ -6,36 +6,38 @@ public class Quick{
   }
   public static int partition (int [] data, int start, int end){
     int randnum = (int)(Math.random() * (end - start) + start);
-    int pivot = data[randnum];
+    switchPlace(data, randnum, end);
+    int pivot = data[end];
+    int upper = end;
+    System.out.println("pivot: " + pivot);
+    //lower needs to smaller than increment.
+    int lower = start - 1;
     /*
     testing purposes
     System.out.println("pivot is" + pivot);
     boolean startTrue = false;
     boolean endTrue = false;*/
-    while (start < end){
-      //finds if number is small enough to move to other side.
-      // doesnt work.
-      while (data[start] < pivot){
-        start++;
-      }
-      while (data[end] > pivot){
-        end--;
-      }
-      if (start < end){
-        switchPlace(data, start, end);
+    for (int increment = start; increment < upper; increment++){
+      if (data[increment] <= pivot){
+        //increment lower because element is smaller than pivot.
+        lower++;
+        switchPlace(data, lower, increment);
       }
     }
+    switchPlace(data, lower + 1, end);
     //return last position of pivot.
-    return end;
+    return lower + 1;
   }
 
   public static void main(String[] args) {
     int[] b = new int[] {13, 23, 14, 37, 40, 93, 2, 17, 12, 9, 87, 44, 68};
+
     System.out.println("\n" + "partition testing" + "\n");
-    System.out.println(partition(b, 0, 12));
+    System.out.println("pivot: " + partition(b, 0, 12));
 
     for (int x = 0; x < b.length; x++){
       System.out.println(b[x]);
     }
+
   }
 }
