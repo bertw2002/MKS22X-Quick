312df95e892122c3d292f274d2d11d651ef6aa47
bertw2002 <awan00@stuy.edu>
2019-03-15 11:47:50 -0400

    some basic code

--- Merge.java
+++ Merge.java
@@ -1,1 +1,14 @@
+public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
 
+  }
+  private static void mergesort(int[]data, int[]temp, int lo, int hi){
+
+  }
+  private static void merge(int[] data1, int[] data2){
+    int length = data1.length + data2.length;
+    int[] finalArr = new int[length];
+    
+  }
+}
